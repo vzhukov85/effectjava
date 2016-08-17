28686ac49540569a726475110e45a8e6e81c42bb
Виктор Ержуков <v.v.erzhukov@gmail.com>
2016-08-17 08:20:45 +0300

    item1

---
+++ item1/src/Services.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Services {
+    private static final Map<String, Provider> providers = new ConcurrentHashMap<String, Provider>();
+    public static final String DEFAULT_PROVIDER = "def";
+
+    public void registerProvider(Provider provider) {
+        registerProvider(DEFAULT_PROVIDER, provider);
+    }
+
+    public void registerProvider(String name, Provider provider) {
+        providers.put(name, provider);
+    }
+
+    public Service newInstance() {
+        return newInstance(DEFAULT_PROVIDER);
+    }
+
+    public Service newInstance(String name) {
+        Provider provider = providers.get(name);
+        if (provider == null) {
+            throw new IllegalArgumentException("No provider register with name: " + name);
+        }
+        return provider.newService();
+    }
+}
