f6249a6b4193e28561540b8b4bb5a39d6f0e246c
Виктор Ержуков <v.v.erzhukov@gmail.com>
2016-10-15 14:48:45 +0300

    item19

---
+++ src/item19/PhysicalConstants.java
@@ -0,0 +1,7 @@
+package item19;
+
+public class PhysicalConstants {
+    public static final double AVOGADROS_NUMBER = 6.0224199e23;
+    public static final double BOLTZMANN_CONSTANT = 1.3806503e-23;
+    public static final double ELECTRON_MASS = 9.10938188e-31;
+}
---
+++ src/item19/PhysicalConstantsBad.java
@@ -0,0 +1,7 @@
+package item19;
+
+public interface PhysicalConstantsBad {
+    double AVOGADROS_NUMBER = 6.0224199e23;
+    double BOLTZMANN_CONSTANT = 1.3806503e-23;
+    double ELECTRON_MASS = 9.10938188e-31;
+}
